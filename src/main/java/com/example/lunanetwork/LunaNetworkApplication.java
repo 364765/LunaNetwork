ac650db0d8368f14703077d2f0a42e90aa7a6ebd
Andreas Dårstad <andreasdaarstad@gmail.com>
2022-11-04 17:23:36 +0100

    Initial commit

---
+++ src/main/java/com/example/lunanetwork/LunaNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.lunanetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunaNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lunanetwork/LunaNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lunanetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunaNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
